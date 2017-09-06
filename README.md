a439d6eaab6180f6dde3da1075771a9e7c2ff454
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2017-09-06 15:55:57 -0400

    added inital files and updated the README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Angular_Partials_Page
 Simply Web Page using Angular for Partials and Bootstrap for components
+<br>
+Simple webpage using Angular to create partials for a 'single page app', Bootstrap for Components and HTML5 boilerplate for the basic html and css.
---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var thisWebApp = function(){
+
+	function Run(){
+		//Angular
+		var app = angular.module('website', ['ngRoute']);
+		app.config( function($routeProvider){
+			$routeProvider
+				.when('/navigation', {template: '/partials/navigation.html'})
+				.when('/header', {template: '/partials/header.html'})
+				.when('/content', {template: '/partials/content.html'})
+				.when('/footer', {template: '/partials/footer.html'})
+				.otherwise({redirectTo:'404', template:'partials/404.html'})
+			});
+		;
+		console.log('RUN');
+	}
+
+	Run();
+
+}();
